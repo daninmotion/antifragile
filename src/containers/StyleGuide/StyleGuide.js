8cd21693c582653b495292a2b935769c6cae9280
Daniel Cardenas <dan85.cardenas@gmail.com>
2019-02-23 08:43:14 -0500

    working on style guide

---
+++ src/containers/StyleGuide/StyleGuide.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class StyleGuide extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          Hello
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+
+export default StyleGuide
